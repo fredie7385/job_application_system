3ee3f7f133c72bd3b6aebc4b775c89536e6b45bf
Fredrick Kabiru <mukii7385@gmail.com>
2024-04-09 16:26:16 +0300

    Initialize

---
+++ src/test/java/com/Freddie/job_application_system/JobApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Freddie.job_application_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
